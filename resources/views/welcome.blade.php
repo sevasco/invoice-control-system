316365f2482f7a1850d4ffb5cdc6fc6f3350b3fb
Sergio Vasco <sergioesteban95@gmail.com>
2019-12-03 17:38:52 -0500

    Login and register working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Invoice Control System
                 </div>
 
                 <div class="links">
